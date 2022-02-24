eb8d801a4babd6dc135b392ad95d83d007bfaa69
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-02-24 13:32:58 -0700

    added the license discription portion

--- index.js
+++ index.js
@@ -55,19 +55,26 @@ inquirer
 
         function renderReadme(title, license, description, install, usage) {
             let licenseBadge = "";
+            let licenseDiscrption = ""
 
             switch (license) {
                 case "MIT": licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+                    licenseDiscrption = "This project is licensed under the MIT license."
                     break;
                 case "APACHE 2.0": licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+                    licenseDiscrption = "This project is licensed under the APACHE 2.0 license."
                     break;
                 case "GPL 3.0": licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+                    licenseDiscrption = "This project is licensed under the GPL 3.0 license."
                     break;
                 case "BSD 3": licenseBadge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+                    licenseDiscrption = "This project is licensed under the BSD 3 license."
                     break;
-                case "none": licenseBadge = "This project uses does not use any licenses"
+                case "none": licenseBadge = ""
+                    licenseDiscrption = "This project is not under any licenses."
                     break;
-                default: licenseBadge = "This project uses does not use any licenses"
+                default: licenseBadge = ""
+                    licenseDiscrption = "This project is not under any licenses."
                     break;
             }
 
@@ -110,6 +117,8 @@ ${install}
 ${usage}
 
 ## License
+
+${licenseDiscrption}
 
 ## Contributing
 
