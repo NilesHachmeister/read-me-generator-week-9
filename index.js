bcf8598f050f01a5e8de74648e7eacb098582967
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-02-24 12:38:35 -0700

    added the switch for the licenses ande the badge gettig put into the readme

--- index.js
+++ index.js
@@ -54,11 +54,31 @@ inquirer
 
 
         function renderReadme(title, license) {
-            let licenseBadge = license;
+            let licenseBadge = "";
+
+            switch (license) {
+                case "MIT": licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+                    break;
+                case "APACHE 2.0": licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+                    break;
+                case "GPL 3.0": licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+                    break;
+                case "BSD 3": licenseBadge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+                    break;
+                case "none": licenseBadge = "none"
+                    break;
+                default: licenseBadge = "none"
+                    break;
+            }
+
+
+
+
 
             return `# ${title}
- 
-            `
+${licenseBadge}
+
+`
         }
 
 
